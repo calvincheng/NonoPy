fee2eedaa78755ac310ed269bafe2d24ddf33993
Calvin Cheng <calvin.cc.cheng@gmail.com>
2020-12-19 19:22:55 +0800

    Add print method for Nonogram class

--- src/main.py
+++ src/main.py
@@ -11,4 +11,29 @@ class Nonogram:
             [0 for _ in range(self.m)] 
             for _ in range(self.n)]
 
+    def __str__(self):
+        string = ""
+        for j in range(self.m):
+            for i in range(self.n):
+                cell = self.grid[j][i]
+                if cell == 0:
+                    string += '. '
+                elif cell == 1:
+                    string += '  '
+                elif cell == 2:
+                    string += '# '
+                else:
+                    raise ValueError(
+                        "Unrecognised value at cell ({}, {}): {}".format(i, j, cell)
+                    )
+            string += "\n"
+        return string
+
+
+N = Nonogram(
+        [[2, 1], [1, 3], [1, 2], [3], [4], [1]], 
+        [[1], [5], [2], [5], [2, 1], [2]]
+    )
+
+print(N)
 
